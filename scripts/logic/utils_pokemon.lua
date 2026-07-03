c752230ca4994182bb14107d89b66c8e252a014e
palex00 <palex000000@gmail.com>
2026-07-03 19:30:30 +0200

    It doesnt actually know about it

--- scripts/logic/utils_pokemon.lua
+++ scripts/logic/utils_pokemon.lua
@@ -260,7 +260,12 @@ function updatePokemon()
     
     for region_key, location in pairs(ENCOUNTER_MAPPING) do
         regionObjects[region_key] = Tracker:FindObjectForCode(location)
-        baseCounts[region_key] = #ENCOUNTERS_GROUPED[region_key]
+        if ENCOUNTERS_GROUPED[region_key] ~= nil then
+            -- Remember to remove this once it is fixed in the apworld
+            baseCounts[region_key] = #ENCOUNTERS_GROUPED[region_key]
+        else
+            print(region_key.." is nil!")
+        end
         pendingDecrements[region_key] = 0
     end
     
@@ -316,7 +321,12 @@ function updatePokemon()
         end
     end
     for region_key, object in pairs(regionObjects) do
-        object.AvailableChestCount = baseCounts[region_key] - pendingDecrements[region_key]
+        if baseCounts[region_key] ~= nil then
+            -- Remember to remove this once it is fixed in the apworld
+            object.AvailableChestCount = baseCounts[region_key] - pendingDecrements[region_key]
+        else
+            print(region_key.." is nil!")
+        end
     end
 
     for _, location in pairs(ENCOUNTER_MAPPING) do
